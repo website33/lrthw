b70e96285ea92b781bc4ece90d1608b1ef660044
website33 <contact@web-sitedesign.fr>
2015-04-22 17:39:29 +0200

    exo wcs

---
+++ ex5.rb
@@ -0,0 +1,18 @@
+my_name = 'Thierry DAMEY'
+my_age = 51 # mais en pleine forme
+my_height = 170 # taille en cm
+my_weight = 80 # poids en kg
+my_eyes = 'Marron'
+my_teeth = 'White'
+my_hair = 'Grey'
+
+puts "let's talk about #{my_name}."
+puts "He's #{my_height} centimeters tall."
+puts "He's #{my_weight} kilograms heavy."
+puts "Actualy it's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}}."
+
